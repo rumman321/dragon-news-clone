c2c50ea81a83565dd8b49df3423e01860532866a
rumman321 <cset-210652@dti.ac>
2024-11-15 17:29:07 +0600

    login form design

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 const AuthLayout = () => {
     return (
-        <div>
-            AuthLayout
+        <div className='font-poppins'>
+            <header className='py-5 w-11/12 mx-auto'>
+                <Navbar></Navbar>
+            </header>
             <Outlet></Outlet>
         </div>
     );
